8785144f948021b99c9838f018b9fb14ecea3653
Tiago Toledo <jtiagotoledo@gmail.com>
2022-07-04 12:35:11 -0300

    Deletando uma Categoria com DELETE

--- src/main/java/com/apolotecnologia/cursomc/resources/exception/ResourceExceptionHandler.java
+++ src/main/java/com/apolotecnologia/cursomc/resources/exception/ResourceExceptionHandler.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.client.HttpServerErrorException;
 
+import com.apolotecnologia.cursomc.services.exceptions.DataIntegrityException;
 import com.apolotecnologia.cursomc.services.exceptions.ObjectNotFoundException;
 
 @ControllerAdvice
@@ -18,5 +19,12 @@ public class ResourceExceptionHandler {
 		
 		StandardError err = new StandardError(HttpStatus.NOT_FOUND.value(), e.getMessage(), System.currentTimeMillis());
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(err);
+	}
+	
+	@ExceptionHandler(DataIntegrityException.class)
+	public ResponseEntity<StandardError> dataIntegrity(DataIntegrityException e, HttpServletRequest request){
+		
+		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), System.currentTimeMillis());
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
 }
--- src/main/java/com/apolotecnologia/cursomc/services/CategoriaService.java
+++ src/main/java/com/apolotecnologia/cursomc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.apolotecnologia.cursomc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.apolotecnologia.cursomc.domain.Categoria;
 import com.apolotecnologia.cursomc.repositories.CategoriaRepository;
+import com.apolotecnologia.cursomc.services.exceptions.DataIntegrityException;
 import com.apolotecnologia.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -29,6 +31,17 @@ public class CategoriaService {
 	public Categoria upDate(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+	
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);			
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que possui produtos");
+			
+		}
 	}
 	
 	
